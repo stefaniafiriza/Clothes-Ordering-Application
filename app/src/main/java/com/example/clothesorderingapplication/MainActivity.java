67c827399cd092109449c6105efe8654e697f4e8
Stefania Firiza <48450919+stefaniafiriza@users.noreply.github.com>
2020-04-24 18:21:33 +0300

    Create connection with the button of Login on the main page COA-26 fixed issue

--- app/src/main/java/com/example/clothesorderingapplication/MainActivity.java
+++ app/src/main/java/com/example/clothesorderingapplication/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.clothesorderingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Button sendToLogin;
+    protected Button sendToRegistration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendToLogin=findViewById(R.id.send_login);
+        sendToRegistration=findViewById(R.id.send_singUp);
+
+        sendToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+
     }
 }
