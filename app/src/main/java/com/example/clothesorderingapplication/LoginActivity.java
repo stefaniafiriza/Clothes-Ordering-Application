2d5c98c6d880ffeecf7c1f2bba86cf61bd0d10e9
Stefania Firiza <48450919+stefaniafiriza@users.noreply.github.com>
2020-04-24 20:32:13 +0300

    Create connection with the registration page COA-27 fixed issue

--- app/src/main/java/com/example/clothesorderingapplication/LoginActivity.java
+++ app/src/main/java/com/example/clothesorderingapplication/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.clothesorderingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    protected TextView Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Register=findViewById(R.id.sendToSignUp);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent indent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(indent);
+            }
+        });
     }
 }
