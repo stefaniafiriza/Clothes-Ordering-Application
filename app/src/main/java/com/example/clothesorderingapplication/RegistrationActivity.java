38607d3d33ede2fe96c3ab3e540f27580c50aca2
Stefania Firiza <48450919+stefaniafiriza@users.noreply.github.com>
2020-04-24 21:57:49 +0300

    Create a specia field for manager COA-42 fixed issue

--- app/src/main/java/com/example/clothesorderingapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/clothesorderingapplication/RegistrationActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     protected TextView Login;
+    protected EditText ManagerCode;
+    protected Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,8 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         Login = findViewById(R.id.sendToLogin);
+        ManagerCode = findViewById(R.id.managercode);
+        aSwitch = findViewById(R.id.switch1);
 
 
         Login.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +30,15 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent indent = new Intent(RegistrationActivity.this, LoginActivity.class);
                 startActivity(indent);
+            }
+        });
+
+        ManagerCode.setVisibility(View.INVISIBLE);
+        aSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ManagerCode.setVisibility(View.VISIBLE);
+                aSwitch.setVisibility(View.INVISIBLE);
             }
         });
     }
