cbab41d0f55901b4c794530bfc88ec1285d022a2
Stefania Firiza <48450919+stefaniafiriza@users.noreply.github.com>
2020-04-27 14:34:52 +0300

    Create the search bar option in the app COA-34

--- app/src/main/java/com/example/clothesorderingapplication/SearchActivity.java
+++ app/src/main/java/com/example/clothesorderingapplication/SearchActivity.java
@@ -2,35 +2,23 @@ package com.example.clothesorderingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
 
     protected MenuItem menuItem_basket, menuItem_favorite;
     protected TextView badgeCounter;
     protected int basketItems = 0;
     protected int favoriteItems = 3;
-    protected Button Products;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        Products=findViewById(R.id.products);
-
-        Products.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this,SearchActivity.class));
-            }
-        });
+        setContentView(R.layout.activity_search);
     }
 
     @Override
