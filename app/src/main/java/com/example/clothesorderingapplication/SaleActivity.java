cdfb08fee7c79679b9eac0a7ac31d12650223cb9
Stefania Firiza <48450919+stefaniafiriza@users.noreply.github.com>
2020-05-04 17:42:41 +0300

    Make an Order COA-41

--- app/src/main/java/com/example/clothesorderingapplication/SaleActivity.java
+++ app/src/main/java/com/example/clothesorderingapplication/SaleActivity.java
@@ -8,7 +8,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SaleActivity extends AppCompatActivity {
 
@@ -16,11 +22,42 @@ public class SaleActivity extends AppCompatActivity {
     protected TextView badgeCounter;
     protected int basketItems = 0;
     protected int favoriteItems = 3;
+    protected ImageButton add1, add2, add3, add4, add5, add6, add7, add8, add9, add10, add11, add12, add13, add14, add15, add16;
+    protected ArrayList<ImageButton> addButton = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sale);
+
+        add1 = findViewById(R.id.add39);
+        add2 = findViewById(R.id.add40);
+        add3 = findViewById(R.id.add41);
+        add4 = findViewById(R.id.add42);
+        add5 = findViewById(R.id.add43);
+        add6 = findViewById(R.id.add44);
+        add7 = findViewById(R.id.add45);
+        add8 = findViewById(R.id.add46);
+        add9 = findViewById(R.id.add48);
+        add10 = findViewById(R.id.add49);
+        add11 = findViewById(R.id.add50);
+        add12 = findViewById(R.id.add51);
+        add13 = findViewById(R.id.add52);
+        add14 = findViewById(R.id.add53);
+        add15 = findViewById(R.id.add54);
+        add16 = findViewById(R.id.add55);
+
+        List<ImageButton> allButton = Arrays.asList(add1, add2, add3, add4, add5, add6, add7, add8, add9, add10, add11, add12, add13, add14, add15, add16);
+        addButton.addAll(allButton);
+
+        for (ImageButton current : addButton) {
+            current.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getApplicationContext(), "Added to shopping basket", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 
     @Override
